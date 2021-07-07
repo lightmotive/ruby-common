b064da459a38c8607c50bda94861f1e02eca792d
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-07 21:07:11 +0000

    Extract common prompt logic for reuse

---
+++ prompt.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative 'validation_error'
+
+# Returns the value from convert_input
+# validate must raise an exception if input is not valid.
+#   Raise ValidationError with helpful explanation (message) to prefix original prompt with custom message.
+def prompt_until_valid(
+  prompt,
+  get_input: -> { gets.chomp },
+  convert_input: ->(input) { input },
+  validate: ->(_value) { nil }
+)
+  prompt(prompt)
+  loop do
+    value = convert_input.call(get_input.call)
+    validate.call(value)
+    break value
+  rescue ValidationError => e
+    prompt("#{e.message} #{prompt}")
+  rescue StandardError
+    prompt("#{MESSAGES['input_invalid_message']} #{prompt}")
+  end
+end
