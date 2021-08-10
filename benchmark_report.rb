e90d5a38babab7460aef042e2d40a7c941eb6589
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-10 22:59:38 +0000

    Implement unlimited benchmark comparisons

--- benchmark_report.rb
+++ benchmark_report.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Refactor this file into a module with a class that hides private functionality.
-# Public module method: benchmark_report
+# TODO: Refactor this file into a module with a class that hides private functionality.
+#   Public module method: benchmark_report
 
 require 'benchmark'
 
@@ -41,18 +41,56 @@ def calculate_speed_difference(faster_details, slower_details)
   (slower_average - faster_average) / slower_average
 end
 
-def report_top_two(implementations, count)
-  sorted_by_total = implementations.sort_by { |implementation| implementation[:total_seconds] }
-  first = build_details(sorted_by_total[0], count)
-  second = build_details(sorted_by_total[1], count)
+# [Implementation label a] was the fastest by p%.
+#
+# [Implementation label a] completed p% (x seconds) faster than [Implementation label b].
+# [Implementation label b] completed p% (x seconds) faster than [Implementation label c].
 
-  puts format('%<faster_label>s was faster than %<slower_label>s by about %<speed_difference>.0f%% ' \
-              '(average %<first_time>.4fs vs %<second_time>.4fs).',
-              faster_label: first[:label],
-              slower_label: second[:label],
-              first_time: first[:average_seconds],
-              second_time: second[:average_seconds],
-              speed_difference: calculate_speed_difference(first, second) * 100)
+def report_format_single(detail)
+  format('%<label>s completed in %<time>.4fs on average.',
+         label: detail[:label],
+         time: detail[:average_seconds])
+end
+
+def report_format_fastest(detail_fastest, detail_next_fastest)
+  format('"%<fastest_label>s" was the fastest by %<speed_difference>.0f%% on average.',
+         fastest_label: detail_fastest[:label],
+         speed_difference: calculate_speed_difference(detail_fastest, detail_next_fastest) * 100)
+end
+
+def report_format_comparison(detail_faster, detail_slower)
+  format('"%<faster_label>s" was faster than "%<slower_label>s" by about %<speed_difference>.0f%% ' \
+    '(average %<first_time>.4fs vs %<second_time>.4fs).',
+         faster_label: detail_faster[:label],
+         slower_label: detail_slower[:label],
+         first_time: detail_faster[:average_seconds],
+         second_time: detail_slower[:average_seconds],
+         speed_difference: calculate_speed_difference(detail_faster, detail_slower) * 100)
+end
+
+def print_fastest(details)
+  if details.size > 1
+    puts "#{report_format_fastest(details[0], details[1])}\n\n"
+  else
+    puts report_format_single(details[0])
+  end
+end
+
+def print_comparison_overview(details)
+  puts '-- Comparison Overview --'
+  details.each_with_index do |detail, idx|
+    break if details[idx + 1].nil?
+
+    puts report_format_comparison(detail, details[idx + 1])
+  end
+end
+
+def report_comparison(implementations, count)
+  implementations = implementations.sort_by { |implementation| implementation[:total_seconds] }
+  details = implementations.map { |implementation| build_details(implementation, count) }
+
+  print_fastest(details) unless details.size.zero?
+  print_comparison_overview(details) if details.size > 1
 end
 
 # test_data structure: { label: { input: ..., expected_output: ... }, ... }
@@ -65,5 +103,5 @@ def benchmark_report(count, iterations_per_count, test_data, implementations)
   end
 
   puts
-  report_top_two(implementations, count)
+  report_comparison(implementations, count)
 end
