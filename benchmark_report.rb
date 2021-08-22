e57eff6b442989911d1be53052b1e02f601b0b4f
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-22 18:27:06 +0000

    Add run_tests method
    - For standardized test runs and output.

--- benchmark_report.rb
+++ benchmark_report.rb
@@ -102,3 +102,9 @@ def benchmark_report(runs, iterations_per_run, tests, implementations)
   puts
   report_comparison(implementations, runs)
 end
+
+def run_tests(tests, callback)
+  tests.each do |test|
+    puts "#{test[:label]}: #{callback.call(test[:input]) == test[:expected_output]}"
+  end
+end
