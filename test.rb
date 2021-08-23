a7f86865186acc6cbf1179280e586edcdb5ac836
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-23 23:50:07 +0000

    Make test data label optional

--- test.rb
+++ test.rb
@@ -4,8 +4,12 @@ def run_tests(test_group_name, tests, callback)
   puts "#{test_group_name} tests:"
 
   tests.each do |test|
-    puts "#{callback.call(test[:input]) == test[:expected_output] ? 'pass' : 'fail'} | #{test[:label]}"
+    test_label = test[:label] || "Input: #{test[:input]}"
+    puts "#{callback.call(test[:input]) == test[:expected_output] ? 'pass' : 'fail'} | #{test_label}"
   rescue StandardError => e
     puts "Error: #{e}"
   end
 end
+
+run_tests('name', [{ label: 'Labeled', input: 1, expected_output: 1 },
+                   { input: 2, expected_output: 2 }], ->(input) { input })
