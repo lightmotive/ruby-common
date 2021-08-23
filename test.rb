c3dc6a6f22fea418c928754e4b4af04b4918e89e
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-23 23:26:25 +0000

    Add test group name parameter

--- test.rb
+++ test.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-def run_tests(tests, callback)
+def run_tests(test_group_name, tests, callback)
+  puts "#{test_group_name} tests:"
+
   tests.each do |test|
     puts "#{callback.call(test[:input]) == test[:expected_output] ? 'pass' : 'fail'} | #{test[:label]}"
   rescue StandardError => e
