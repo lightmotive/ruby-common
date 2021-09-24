3b35ec226d0b7e53c2f084a427dc7ac2f7827335
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-24 23:13:30 +0000

    Refactor prompt loop to separate method

--- prompt.rb
+++ prompt.rb
@@ -7,12 +7,24 @@ require_relative 'validation_error'
 # Returns the value from options[:convert_input]
 # options[:validate]: if input is invalid, raise ValidationError with custom message or StandardError without message.
 #   Raise ValidationError with helpful explanation (message) to prefix original.
-def prompt_until_valid_apply_default_options(options)
+def prompt_until_valid_apply_default_options!(options)
   options[:prompt_with_format] ||= ->(msg) { puts "-> #{msg}" }
   options[:input_invalid_default_message] ||= 'Invalid input.'
   options[:get_input] ||= -> { gets.chomp }
   options[:convert_input] ||= ->(input) { input }
   options[:validate] ||= ->(_input_converted) { nil }
+end
+
+def _prompt_until_valid_loop(message, options)
+  loop do
+    value = options[:convert_input].call(options[:get_input].call)
+    options[:validate].call(value)
+    break value
+  rescue ValidationError => e
+    options[:prompt_with_format].call("#{e.message} #{message}")
+  rescue StandardError
+    options[:prompt_with_format].call("#{options[:input_invalid_default_message]} #{message}")
+  end
 end
 
 def prompt_until_valid(
@@ -25,16 +37,8 @@ def prompt_until_valid(
     validate: nil
   }
 )
-  prompt_until_valid_apply_default_options(options)
+  prompt_until_valid_apply_default_options!(options)
 
   options[:prompt_with_format].call(message)
-  loop do
-    value = options[:convert_input].call(options[:get_input].call)
-    options[:validate].call(value)
-    break value
-  rescue ValidationError => e
-    options[:prompt_with_format].call("#{e.message} #{message}")
-  rescue StandardError
-    options[:prompt_with_format].call("#{options[:input_invalid_default_message]} #{message}")
-  end
+  _prompt_until_valid_loop(message, options)
 end
